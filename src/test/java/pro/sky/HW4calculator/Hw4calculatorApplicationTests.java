06f25d6e6cad496f9acfdffb8db78656783b3414
MaraOD555 <ruffbc@hotmail.com>
2022-07-15 18:31:21 +0300

    master commit

---
+++ src/test/java/pro/sky/HW4calculator/Hw4calculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HW4calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw4calculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
